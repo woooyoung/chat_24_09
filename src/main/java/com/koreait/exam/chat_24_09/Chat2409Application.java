fc6c546c9e9a40071793e7f55ab8ed173410a976
woooyoung <axdsw121@gmail.com>
2024-09-05 16:26:59 +0900

    PostMan, Post /chat/writeMessage

--- src/main/java/com/koreait/exam/chat_24_09/Chat2409Application.java
+++ src/main/java/com/koreait/exam/chat_24_09/Chat2409Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Chat2409Application {
-
     public static void main(String[] args) {
+
         SpringApplication.run(Chat2409Application.class, args);
     }
-
 }
---
+++ src/main/java/com/koreait/exam/chat_24_09/ChatController.java
@@ -0,0 +1,15 @@
+package com.koreait.exam.chat_24_09;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public String writeMessage() {
+        return "메세지 작성됨";
+    }
+}
