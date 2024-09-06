780f106baac2b62b0b04ecd120aa90a2170135f8
woooyoung <axdsw121@gmail.com>
2024-09-06 15:23:23 +0900

    messages filtering

--- src/main/java/com/koreait/exam/chat_24_09/ChatController.java
+++ src/main/java/com/koreait/exam/chat_24_09/ChatController.java
@@ -3,14 +3,17 @@ package com.koreait.exam.chat_24_09;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
@@ -20,10 +23,6 @@ public class ChatController {
 
 
     public record writeMessageResponse(long id) {
-    }
-
-
-    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
     }
 
 
@@ -40,12 +39,38 @@ public class ChatController {
         );
     }
 
+    public record messagesRequest(Long fromId) {
+    }
+
+    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
+    }
+
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<messagesResponse> messages() {
+    public RsData<messagesResponse> messages(messagesRequest req) {
+
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req : {}", req);
+
+        // 번호가 같이 입력되었다면???
+        if (req.fromId != null) {
+            // 해당 번호의 채팅 메세지가 전체 리스트의 몇번째 인덱스인지? 없다면 -1
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 -1이 아니라면? 0번부터 index번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
+
         return new RsData<>("S-1",
                 "성공",
-                new messagesResponse(chatMessages, chatMessages.size())
+                new messagesResponse(messages, messages.size())
         );
     }
 }
